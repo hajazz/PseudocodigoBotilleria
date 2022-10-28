3eca7163909b3fc84070d0ae96adec4490c430d6
hajazz <116355562+hajazz@users.noreply.github.com>
2022-10-28 10:13:41 -0300

    Create Botilleria.py

---
+++ Botilleria.py
@@ -0,0 +1,17 @@
+class Product {
+  ##atributos##
+  name_product='' #nombre del producto
+  date_of_admission='' #fecha de ingreso 
+  expiration_date='' #fecha de vencimiento
+  product_ observation='' #observación del producto
+  serial_number='' #numero de serie
+  bar_code='' #codigo de barras
+  drinkable='' #bebestibe
+  consumable='' #consumible
+  grade_alcohol='' #grados de alcohol
+  price='' #precio
+  ##fin atributos##
+  
+  ##metódos##
+  
+  ##fin metódos##
